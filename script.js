bd7229a83232371babdbb4cf168f13a6f059273d
Guilherme Medon <113711968+Gmedon@users.noreply.github.com>
2022-10-25 12:16:08 -0300

    ADD caracteres especiais

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ alfabeto[43] = "+"
 alfabeto[61] = "="
 alfabeto[60] = "<"
 alfabeto[62] = ">" 
-alfabeto[65] = "A"
 let complete = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "q", "q",
 "t", "u", "v", "w", "x", "y", "z"]
 let alfabetoM = [ ...complete, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
@@ -34,19 +33,24 @@ function cod() {
                 for(let i = 0; i < texto.length; i++) {
                         numeroscifra.push(texto.charCodeAt(i))
                 }
+                console.log(numeroscifra)
                 for(let j in numeroscifra) {
                         if(numeroscifra[j] >= 97) {
                                 numerossomadoscifra.push((numeroscifra[j] - 97 + qpassos1)%26)
                         }else if(numeroscifra[j] >= 65 && numeroscifra[j] <= 90) {
                                 numerossomadoscifra.push(((numeroscifra[j] - 65 + qpassos1)%26)+26)
+                        }else if(numeroscifra[j] >= 30 && numeroscifra[j] <= 64) {
+                                numerossomadoscifra.push(numeroscifra[j])
                         }
                 }
                 console.log(numerossomadoscifra)
                 for(let f = 0; f < numerossomadoscifra.length; f++) {
-                        if(numerossomadoscifra[f] <= 25){
+                        if(numeroscifra[f] >= 96){
                                 exibircifra.push(alfabeto[numerossomadoscifra[f]])
-                        }else if(numerossomadoscifra[f] >= 26 && numerossomadoscifra[f] <= 51) {
+                        }else if(numeroscifra[f] >= 65) {
                                 exibircifra.push(alfabetoM[numerossomadoscifra[f]])
+                        }else if (numeroscifra[f] <= 64) {
+                                exibircifra.push(alfabeto[numeroscifra[f]])
                         }
                 }
                 campoTexto[1].setAttribute("value", exibircifra.join(""))
@@ -72,14 +76,18 @@ function deco() {
                                 numerossomadoscifra.push((numeroscifra[j] - 97 + 26 - qpassos1)%26)
                         }else if(numeroscifra[j] >= 65 && numeroscifra[j] <= 90) {
                                 numerossomadoscifra.push(((numeroscifra[j] - 65 + 26 - qpassos1)%26)+26)
+                        }else if(numeroscifra[j] >= 30 && numeroscifra[j] <= 64) {
+                                numerossomadoscifra.push(numeroscifra[j])
                         }
                 }
                 console.log(numerossomadoscifra)
                 for(let f = 0; f < numerossomadoscifra.length; f++) {
-                        if(numerossomadoscifra[f] <= 25){
+                        if(numeroscifra[f] >= 96){
                                 exibircifra.push(alfabeto[numerossomadoscifra[f]])
-                        }else if(numerossomadoscifra[f] >= 26 && numerossomadoscifra[f] <= 51) {
+                        }else if(numeroscifra[f] >= 65) {
                                 exibircifra.push(alfabetoM[numerossomadoscifra[f]])
+                        }else if (numeroscifra[f] <= 64) {
+                                exibircifra.push(alfabeto[numeroscifra[f]])
                         }
                 }
                 campoTexto[1].setAttribute("value", exibircifra.join(""))
